0390e338df0fb53059f0b6e9c14ad4cefdcb0b6c
Mário Feroldi <mferoldif@gmail.com>
2020-04-06 03:18:21 -0300

    Parse infix, postfix and mixfix operators

--- pratt.py
+++ pratt.py
@@ -22,6 +22,25 @@ class TokenKind(IntEnum):
     EOF = auto()
 
 
+def punctuator(tok_kind):
+    tok_to_str = {
+        TokenKind.LEFT_PAREN: "(",
+        TokenKind.RIGHT_PAREN: ")",
+        TokenKind.COMMA: ",",
+        TokenKind.ASSIGN: "=",
+        TokenKind.PLUS: "+",
+        TokenKind.MINUS: "-",
+        TokenKind.ASTERISK: "*",
+        TokenKind.SLASH: "/",
+        TokenKind.CARET: "^",
+        TokenKind.TILDE: "~",
+        TokenKind.BANG: "!",
+        TokenKind.QUESTION: "?",
+        TokenKind.COLON: ":",
+    }
+    return tok_to_str.get(tok_kind)
+
+
 @dataclass
 class Token:
     kind: TokenKind
@@ -71,36 +90,24 @@ class Lexer:
         return tokens
 
 
-def punctuator(tok_kind):
-    tok_to_str = {
-        TokenKind.LEFT_PAREN: "(",
-        TokenKind.RIGHT_PAREN: ")",
-        TokenKind.COMMA: ",",
-        TokenKind.ASSIGN: "=",
-        TokenKind.PLUS: "+",
-        TokenKind.MINUS: "-",
-        TokenKind.ASTERISK: "*",
-        TokenKind.SLASH: "/",
-        TokenKind.CARET: "^",
-        TokenKind.TILDE: "~",
-        TokenKind.BANG: "!",
-        TokenKind.QUESTION: "?",
-        TokenKind.COLON: ":",
-    }
-    return tok_to_str.get(tok_kind)
-
-
 class Parser:
-    def __init__(self, tokens, prefix_parsers):
+    def __init__(self, tokens, prefix_parsers, infix_parsers):
         self.tokens = tokens
         self.tok_buffer = []
         self.prefix_parsers = prefix_parsers
+        self.infix_parsers = infix_parsers
 
     def parse_expr(self):
         tok = self.consume()
         prefix_parser = self.prefix_parsers.get(tok.kind)
         if prefix_parser:
-            return prefix_parser.parse(self, tok)
+            lhs = prefix_parser.parse(self, tok)
+            infix_parser = self.infix_parsers.get(self.look_ahead().kind)
+            if infix_parser:
+                tok = self.consume()
+                return infix_parser.parse(self, lhs, tok)
+            else:
+                return lhs
         else:
             raise RuntimeError(f"could not parse {tok.kind}")
 
@@ -126,6 +133,9 @@ class Parser:
         return self.tok_buffer[distance]
 
 
+# AST
+
+
 class Expr:
     def dump(self) -> str:
         pass
@@ -138,6 +148,15 @@ class NameExpr(Expr):
     def dump(self):
         return self.value
 
+
+@dataclass
+class GroupExpr(Expr):
+    inner: Expr
+
+    def dump(self):
+        return "(" + self.inner.dump() + ")"
+
+
 @dataclass
 class PrefixOpExpr(Expr):
     op: TokenKind
@@ -147,9 +166,50 @@ class PrefixOpExpr(Expr):
         return "(" + punctuator(self.op) + self.rhs.dump() + ")"
 
 
+@dataclass
+class InfixOpExpr(Expr):
+    op: TokenKind
+    lhs: Expr
+    rhs: Expr
+
+    def dump(self):
+        return "(" + self.lhs.dump() + punctuator(self.op) + self.rhs.dump() + ")"
+
+
+@dataclass
+class PostfixOpExpr(Expr):
+    op: TokenKind
+    lhs: Expr
+
+    def dump(self):
+        return "(" + self.lhs.dump() + punctuator(self.op) + ")"
+
+
+@dataclass
+class ConditionalExpr(Expr):
+    condition: Expr
+    then_arm: Expr
+    else_arm: Expr
+
+    def dump(self):
+        return (
+            "("
+            + self.condition.dump()
+            + "?"
+            + self.then_arm.dump()
+            + ":"
+            + self.else_arm.dump()
+            + ")"
+        )
+
+
+# Prefix operator parsers
+
+
 class PrefixParser:
     def parse(self, parser: Parser, token: Token) -> Expr:
         pass
+
 
 class NameParser(PrefixParser):
     def parse(self, parser, token):
@@ -162,6 +222,45 @@ class PrefixOpParser(PrefixParser):
         return PrefixOpExpr(op=token.kind, rhs=operand)
 
 
+class GroupParser(PrefixParser):
+    def parse(self, parser, token):
+        assert token.kind == TokenKind.LEFT_PAREN
+        inner = parser.parse_expr()
+        parser.consume(expected=TokenKind.RIGHT_PAREN)
+        return GroupExpr(inner=inner)
+
+
+# Infix, postfix and mixfix operator parsers
+
+
+# Also used for postfix and mixfix.
+class InfixParser:
+    def parse(self, parser: Parser, lhs: Expr, token: Token) -> Expr:
+        pass
+
+
+class InfixOpParser(InfixParser):
+    def parse(self, parser, lhs, token):
+        rhs = parser.parse_expr()
+        return InfixOpExpr(op=token.kind, lhs=lhs, rhs=rhs)
+
+
+class PostfixOpParser(InfixParser):
+    def parse(self, parser, lhs, token):
+        return PostfixOpExpr(op=token.kind, lhs=lhs)
+
+
+class ConditionalParser(InfixParser):
+    def parse(self, parser, lhs, token):
+        assert token.kind == TokenKind.QUESTION
+        then_arm = parser.parse_expr()
+        parser.consume(expected=TokenKind.COLON)
+        else_arm = parser.parse_expr()
+        return ConditionalExpr(condition=lhs, then_arm=then_arm, else_arm=else_arm)
+
+
+# Main
+
 if __name__ == "__main__":
     lexer = Lexer(input())
     prefix_parsers = {
@@ -170,7 +269,16 @@ if __name__ == "__main__":
         TokenKind.MINUS: PrefixOpParser(),
         TokenKind.TILDE: PrefixOpParser(),
         TokenKind.BANG: PrefixOpParser(),
+        TokenKind.LEFT_PAREN: GroupParser(),
     }
-    parser = Parser(lexer, prefix_parsers)
+    infix_parsers = {
+        TokenKind.PLUS: InfixOpParser(),
+        TokenKind.MINUS: InfixOpParser(),
+        TokenKind.ASTERISK: InfixOpParser(),
+        TokenKind.SLASH: InfixOpParser(),
+        TokenKind.CARET: InfixOpParser(),
+        TokenKind.BANG: PostfixOpParser(),
+        TokenKind.QUESTION: ConditionalParser(),
+    }
+    parser = Parser(lexer, prefix_parsers, infix_parsers)
     print(parser.parse_expr().dump())
-
